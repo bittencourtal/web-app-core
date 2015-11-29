bbb1a21f3e2b77358aea004784b5c717aa5fcd9c
Joao M <joaom182@hotmail.com>
2015-11-29 14:09:44 -0200

    Remove a pasta bower_components e coloca no .gitignore

--- bower_components/dss/src/interface/dss.init.js
+++
@@ -1,9 +0,0 @@
-//File : src/interface/dss.init.js
-
-(function(dss){
-'use strict';
-	
-	dss.core.defineMethod('init',dss.core.fetchStyleSheets);
-	document.addEventListener('DOMContentLoaded', dss.init, false);
-
-})(this.dss);
